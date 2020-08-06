beaa08b96ac678ba16b9aa6fc3ddc5ffa6f22b9b
Niels Swimberghe <nielsswimberghe@gmail.com>
2020-08-06 15:00:47 -0400

    Add project files.

---
+++ DnnAuthorizationFilter.cs
@@ -0,0 +1,21 @@
+﻿using Hangfire.Dashboard;
+using Microsoft.Owin;
+using DotNetNuke.Entities.Users;
+
+namespace DnnHangfireIntegration
+{
+    public class DnnAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var owinContext = new OwinContext(context.GetOwinEnvironment());
+            if (!owinContext.Authentication.User.Identity.IsAuthenticated)
+            {
+                return false;
+            }
+
+            var userInfo = UserController.GetUserByName(owinContext.Authentication.User.Identity.Name);
+            return userInfo.IsSuperUser;
+        }
+    }
+}
---
+++ DnnOwinStartup.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using Hangfire.Dashboard;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DnnHangfireIntegration.Startup))]
+
+namespace DnnHangfireIntegration
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("HangfireConnection");
+            app.UseHangfireServer();
+            app.UseHangfireDashboard("/desktopmodules/hangfire", new DashboardOptions
+            {
+                Authorization = new[] { new DnnAuthorizationFilter() }
+            });
+        }
+    }
+}
